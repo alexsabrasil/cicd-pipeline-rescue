06fa542cff28b49018ae123789ed3593364508fb
alexsabrasil <alexsa.tavares@gmail.com>
2026-09-01 18:00:36 -0300

    ci: adiciona pipeline inicial com testes lint e docker build

--- eslint.config.js
+++ eslint.config.js
@@ -13,7 +13,9 @@ module.exports = defineConfig([
         module: 'readonly',
         describe: 'readonly',
         it: 'readonly',
-        expect: 'readonly'
+        test: 'readonly',
+        expect: 'readonly',
+        fetch: 'readonly'
       }
     },
     rules: {
